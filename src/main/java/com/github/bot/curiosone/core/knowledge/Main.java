8e8634c1b761ef783c0277971e5f7a2bb4b088dc
Christian Sordi <christian.sordi@hotmail.it>
2017-07-17 11:35:41 +0200

    Eliminato main di prova

--- src/main/java/com/github/bot/curiosone/core/knowledge/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public boolean someLibraryMethod() {
-    return true;
-  }
-  public static void main(String[] args) {
-    System.out.println("Hell World");
-    System.out.println("vedemo se se cambia anche su github");
-  }
-}
