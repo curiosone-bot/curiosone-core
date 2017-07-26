aabb911be33010269d37874e61b7b3acfc8136bd
riva <riva@riva-16.04>
2017-07-26 16:30:50 +0200

    Restore IWord interface

--- src/main/java/com/github/bot/curiosone/core/nlp/tokenizer/interfaces/IWord.java
+++ src/main/java/com/github/bot/curiosone/core/nlp/tokenizer/interfaces/IWord.java
@@ -1,12 +1,15 @@
-	public boolean isKnown();from Dictionary
-	 * @author riva
-	 */
-	public String getValue();t> getSynsets();t.curiosone.core.nlp.tokenizer.interfaces;
+package com.github.bot.curiosone.core.nlp.tokenizer.interfaces;
 
 import java.util.List;
 
-	String getValue();
-	List<ISynset> getSynsets();
-	String getLemma();
-	boolean isKnown();
+public interface IWord {
+	/**
+	 * Word info from Dictionary
+	 * @author riva
+	 */
+	public String getValue();
+	public List<ISynset> getSynsets();
+	public String getLemma();
+	public boolean isKnown();
+}
 }
