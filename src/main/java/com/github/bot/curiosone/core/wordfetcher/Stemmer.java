52034c2bad9e0e7a249d2504a82129d7da26e456
Claudio Venanzi <cl124c41a@gmail.com>
2017-07-27 15:26:54 +0200

    little code cleanup

--- src/main/java/com/github/bot/curiosone/core/wordfetcher/Stemmer.java
+++ src/main/java/com/github/bot/curiosone/core/wordfetcher/Stemmer.java
@@ -1,37 +1,30 @@
 package com.github.bot.curiosone.core.wordfetcher;
 
+import edu.mit.jwi.Dictionary;
+import edu.mit.jwi.IDictionary;
+import edu.mit.jwi.morph.WordnetStemmer;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
-import edu.mit.jwi.Dictionary;
-import edu.mit.jwi.IDictionary;
-import edu.mit.jwi.item.IIndexWord;
-import edu.mit.jwi.item.IWord;
-import edu.mit.jwi.item.IWordID;
-import edu.mit.jwi.item.POS;
-import edu.mit.jwi.morph.IStemmer;
-import edu.mit.jwi.morph.WordnetStemmer;
-
 public class Stemmer {
-
-    /**
-     * 
-     * @param s
-     * @return
-     * @throws IOException
-     */
-    public static String toStem(String input) throws IOException {
+  
+  /**
+   * Get a base form of a word.
+   * @param input non-base word
+   * @return base word
+   * @throws IOException stuff may go wrong
+   */
+  public static String toStem(String input) throws IOException {
       
-      String path = "src/main/res/dict";
+    String path = "src/main/res/dict";
       
-      System.out.println("a");
-      IDictionary dict = new Dictionary(new URL("file", null , path)); dict.open();
-      System.out.println("b");
-      List<String> stems =  new WordnetStemmer(dict).findStems(input, null);
-      System.out.println("c");
-      
-      return stems.get(stems.size() - 1);
-    }
+    IDictionary dict = new Dictionary(new URL("file", null , path));
+    dict.open();
     
+    List<String> stems =  new WordnetStemmer(dict).findStems(input, null);
+      
+    return stems.get(stems.size() - 1);
+  }  
 }
