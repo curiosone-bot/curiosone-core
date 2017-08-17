c70c2cb34b4a1e64797da0d918e0a2299a0999ca
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-08-17 22:15:42 +0200

    SemanticRelationType and Graph codestyle fix

--- src/main/java/com/github/bot/curiosone/core/knowledge/Interfaces/Graph.java
+++ src/main/java/com/github/bot/curiosone/core/knowledge/Interfaces/Graph.java
@@ -1,32 +1,31 @@
 package com.github.bot.curiosone.core.knowledge.interfaces;
 
+import com.github.bot.curiosone.core.knowledge.SemanticRelationType;
 import com.github.bot.curiosone.core.knowledge.interfaces.Edge;
 
 import java.util.Collection;
 import java.util.Set;
 
-import com.github.bot.curiosone.core.knowledge.SemanticRelationType;
-
 /**
- * Interfaccia che rappresenta un grafo
+ * Interfaccia che rappresenta un grafo.
  * @author navigli
  *
  */
-public interface Graph{
+public interface Graph {
   /**
-   * Aggiunge un arco
+   * Aggiunge un arco.
    * @param e l'arco da aggiungere
    */
   void add(Edge e);
 
   /**
-   * Aggiunge un vertice
+   * Aggiunge un vertice.
    * @param v il vertice da aggiungere
    */
   void add(Vertex v);
 
   /**
-   * Aggiunge un arco da v1 a v2 del tipo specificato
+   * Aggiunge un arco da v1 a v2 del tipo specificato.
    * @param v1 sorgente
    * @param v2 destinazione
    * @param type tipo dell'arco
@@ -34,40 +33,40 @@ public interface Graph{
   void addEdge(Vertex v1, Vertex v2, SemanticRelationType type);
 
   /**
-   * Restituisce true se l'arco e' contenuto nel grafo
+   * Restituisce true se l'arco e' contenuto nel grafo.
    * @param e l'arco da verificare
    * @return true se l'arco e' contenuto, false altrimenti
    */
   boolean containsEdge(Edge e);
 
   /**
-   * Restituisce true se il vertice e' contenuto nel grafo
+   * Restituisce true se il vertice e' contenuto nel grafo.
    * @param v il vertice da verificare
    * @return true se il vertice e' contenuto, false altrimenti
    */
   boolean containsVertex(Vertex v);
 
   /**
-   * Restituisce una copia dell'insieme degli archi
+   * Restituisce una copia dell'insieme degli archi.
    * @return l'insieme degli archi del grafo
    */
   Set<Edge> edgeSet();
 
   /**
-   * Restituisce una copia dell'insieme dei vertici
+   * Restituisce una copia dell'insieme dei vertici.
    * @return l'insieme dei vertici
    */
   Set<Vertex> vertexSet();
 
   /**
-   * Restituisce l'insieme di archi uscenti
+   * Restituisce l'insieme di archi uscenti.
    * @param v vertice su cui calcolare l'insieme di archi
    * @return insieme degli archi uscenti
    */
   Set<Edge> outgoingEdges(Vertex v);
 
   /**
-   * Aggiunge tutti i vertici forniti in input
+   * Aggiunge tutti i vertici forniti in input.
    * @param vertexSet i vertici da aggiungere
    */
   void addVertices(Collection<? extends Vertex> vertexSet);
@@ -79,9 +78,9 @@ public interface Graph{
   void addEdges(Collection<? extends Edge> edgeSet);
 
   /**
+   * Returns the linked vertex, if present.
    * @param tipo del collegamento dell'arco da cercare
    * @param token vertice del quale cercare il significato collegato
-   * @return vertice collegato se presente
    */
   Vertex search(SemanticRelationType tipo, String token);
 }
--- src/main/java/com/github/bot/curiosone/core/knowledge/SemanticRelationType.java
+++ src/main/java/com/github/bot/curiosone/core/knowledge/SemanticRelationType.java
@@ -1,19 +1,20 @@
 package com.github.bot.curiosone.core.knowledge;
+
 /**
- *
- * @author Christian
- *
+ * Semantic Relation Type that can be used between two Concepts.
+ * @author Christian Sordi
  */
-public enum SemanticRelationType
-{
-  /** Identificatori univoci dell'enumerazioni */
-  HYPERONYM,		// e' un iperonimo
-  ANTONYM,		// e' il contrario di
-  HYPONYM,		// e' un iponimo
-  MERONYM_PART,	// e' parte di
-  SIMILIAR_TO,	// simile a
-  USAGE,			// viene usato per
-  TIME,			// Data
+public enum SemanticRelationType {
+  /**
+   * Identificatori univoci dell'enumerazioni.
+   */
+  HYPERONYM, // e' un iperonimo
+  ANTONYM, // e' il contrario di
+  HYPONYM, // e' un iponimo
+  MERONYM_PART, // e' parte di
+  SIMILIAR_TO, // simile a
+  USAGE, // viene usato per
+  TIME, // Data
 
   // sono solo una parte dei Pointer relativi a Wordnet
 }
