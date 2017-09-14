806260479768868b2f870829d7cb792e18e1711a
simonepri <simone.pri@hotmail.it>
2017-09-14 20:39:05 +0200

    Clean PosT

--- src/main/java/com/github/bot/curiosone/core/nlp/tokenizer/PosT.java
+++ src/main/java/com/github/bot/curiosone/core/nlp/tokenizer/PosT.java
@@ -1,23 +1,37 @@
 package com.github.bot.curiosone.core.nlp.tokenizer;
 
-/**
- * Part Of Speech (POS) type.
- *
- * @author Andrea Rivitto && Eugenio Schintu
- * @see https://en.oxforddictionaries.com/grammar/word-classes-or-parts-of-speech
- */
-
+ /**
+  * Part Of Speech (POS) type.
+  * <li>{@link #AP}</li>
+  * <li>{@link #APP}</li>
+  * <li>{@link #ADJ}</li>
+  * <li>{@link #CONJ}</li>
+  * <li>{@link #DET}</li>
+  * <li>{@link #INTERJ}</li>
+  * <li>{@link #N}</li>
+  * <li>{@link #NEG}</li>
+  * <li>{@link #NP}</li>
+  * <li>{@link #NPP}</li>
+  * <li>{@link #NUMB}</li>
+  * <li>{@link #PREP}</li>
+  * <li>{@link #PRON}</li>
+  * <li>{@link #S}</li>
+  * <li>{@link #UNKN}</li>
+  * <li>{@link #V}</li>
+  * <li>{@link #VP}</li>
+  * <li>{@link #VPP}</li>
+  * @see https://en.oxforddictionaries.com/grammar/word-classes-or-parts-of-speech
+  */
 public enum PosT {
+  /**
+   * Adjective Phrase.
+   */
+  AP,
 
   /**
-   * Nominal part.
+   * Adjective preposition phrase.
    */
-  NP,
-
-  /**
-   * Verbal part.
-   */
-  VP,
+  APP,
 
   /**
    * Adjective.
@@ -30,16 +44,6 @@ public enum PosT {
   ADV,
 
   /**
-   * Pronoun.
-   */
-  PRON,
-
-  /**
-   * Preposition.
-   */
-  PREP,
-
-  /**
    * Conjunction.
    */
   CONJ,
@@ -50,9 +54,11 @@ public enum PosT {
   DET,
 
   /**
-   * Verb preposition phrase.
+   * Interjections.
+   * (bye, goodbye, hello, farewell, hi, so long excuse me, thanks, thank you,
+   *  thanks a lot, sorry, pardon)
    */
-  VPP,
+  INTERJ,
 
   /**
    * Noun.
@@ -60,19 +66,19 @@ public enum PosT {
   N,
 
   /**
-   * Noun preposition phrase.
-   */
-  NPP,
-
-  /**
    * Negation.
    */
   NEG,
 
   /**
-   * Verb.
+   * Nominal part.
    */
-  V,
+  NP,
+
+  /**
+   * Noun preposition phrase.
+   */
+  NPP,
 
   /**
    * Number.
@@ -80,9 +86,14 @@ public enum PosT {
   NUMB,
 
   /**
-   * Adjective Phrase.
+   * Preposition.
    */
-  AP,
+  PREP,
+
+  /**
+   * Pronoun.
+   */
+  PRON,
 
   /**
    * Sentence.
@@ -90,18 +101,22 @@ public enum PosT {
   S,
 
   /**
-   * Adjective preposition phrase.
-   */
-  APP,
-
-  /**
-   * Interjections (bye, goodbye, hello, farewell, hi, so long excuse me,
-   *    thanks, thank you, thanks a lot, sorry, pardon).
-   */
-  INTERJ,
-
-  /**
    * Unknown.
    */
   UNKN
+
+  /**
+   * Verb.
+   */
+  V,
+
+  /**
+   * Verbal part.
+   */
+  VP,
+
+  /**
+   * Verb preposition phrase.
+   */
+  VPP,
 }
