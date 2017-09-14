43f31eca2ccad0d6a1746ddae003ab6eb6b6d7f1
simonepri <simone.pri@hotmail.it>
2017-09-14 18:13:05 +0200

    Clean spacing

--- src/main/java/com/github/bot/curiosone/core/nlp/tokenizer/PosT.java
+++ src/main/java/com/github/bot/curiosone/core/nlp/tokenizer/PosT.java
@@ -12,115 +12,96 @@ public enum PosT {
   /**
    * Nominal part.
    */
-
   NP,
 
   /**
    * Verbal part.
    */
-
   VP,
 
   /**
    * Adjective.
    */
-
   ADJ,
 
   /**
    * Adverb.
    */
-
   ADV,
 
   /**
    * Pronoun.
    */
-
   PRON,
 
   /**
    * Preposition.
    */
-
   PREP,
 
   /**
    * Conjunction.
    */
-
   CONJ,
 
   /**
    * Determiner.
    */
-
   DET,
 
   /**
    * Verb preposition phrase.
    */
-
   VPP,
 
   /**
    * Noun.
    */
-
   N,
 
   /**
    * Noun preposition phrase.
    */
-
   NPP,
 
   /**
    * Negation.
    */
-
   NEG,
 
   /**
    * Verb.
    */
-
   V,
 
   /**
    * Number.
    */
-
   NUMB,
 
   /**
    * Adjective Phrase.
    */
-
   AP,
 
   /**
    * Sentence.
    */
-
   S,
 
   /**
    * Adjective preposition phrase.
    */
-
   APP,
 
   /**
    * Interjections (bye, goodbye, hello, farewell, hi, so long excuse me,
    *    thanks, thank you, thanks a lot, sorry, pardon).
    */
-
   INTERJ,
 
   /**
    * Unknown.
    */
-
   UNKN
 }
