667af58bc12d5668361ca60f65b47b05f08237ce
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-15 22:14:31 +0200

    fix PhraseType class codestyle

--- src/main/java/com/github/bot/curiosone/core/nlp/base/PhraseType.java
+++ src/main/java/com/github/bot/curiosone/core/nlp/base/PhraseType.java
@@ -1,32 +1,36 @@
 package com.github.bot.curiosone.core.nlp.base;
 
 /**
- * Different typology of sentence.
- */
+* Different typology of sentence.
+*/
 
 public enum PhraseType {
 
   /**
-   * Sentence in input is a question.
-   */
+  * Sentence in input is a question.
+  */
   QUESTION,
 
   /**
-   * Sentence in input is an affirmation.
-   */
+  * Sentence in input is an affirmation.
+  */
   AFFIRMATION,
 
   /**
-   * Sentence in input is an exclamation.
-   */
+  * Sentence in input is an exclamation.
+  */
   EXCLAMATION;
 
+  /**
+  * Returns the PhraseType for the given input phrase.
+  * @param str the Phrase to be examinated.
+  */
   public static PhraseType get(String str) {
     char c = str.charAt(str.length() - 1);
     switch (c) {
       case '?': return QUESTION;
       case '!': return EXCLAMATION;
+      default: return AFFIRMATION;
     }
-    return AFFIRMATION;
   }
 }
