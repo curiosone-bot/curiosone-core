b804f7eab2cd5d0c6e736eeff6d516704b8d1352
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-16 09:37:14 +0200

    improve and fix javadoc in PhraseType class

--- src/main/java/com/github/bot/curiosone/core/nlp/base/PhraseType.java
+++ src/main/java/com/github/bot/curiosone/core/nlp/base/PhraseType.java
@@ -7,24 +7,26 @@ package com.github.bot.curiosone.core.nlp.base;
 public enum PhraseType {
 
   /**
-  * Sentence in input is a question.
-  */
+   * Sentence in input is a question.
+   */
   QUESTION,
 
   /**
-  * Sentence in input is an affirmation.
-  */
+   * Sentence in input is an affirmation.
+   */
   AFFIRMATION,
 
   /**
-  * Sentence in input is an exclamation.
-  */
+   * Sentence in input is an exclamation.
+   */
   EXCLAMATION;
 
   /**
-  * Returns the PhraseType for the given input phrase.
-  * @param str the Phrase to be examinated.
-  */
+   * Gets the phrase type for the given phrase.
+   * @param str a String containing the Phrase to be examinated.
+   * @return a String containing the phrase type for the given phrase.
+   * @see PhraseType
+   */
   public static PhraseType get(String str) {
     char c = str.charAt(str.length() - 1);
     switch (c) {
