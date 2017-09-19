31bd877ab325bb7af94400ba1305ee798d72371e
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-19 10:07:17 +0200

    add FrequencyTest test class

---
+++ src/test/java/com/github/bot/curiosone/core/nlp/FrequencyTest.java
@@ -0,0 +1,12 @@
+package com.github.bot.curiosone.core.nlp;
+
+// SUPPRESS CHECKSTYLE AvoidStarImport
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class FrequencyTest {
+  
+}
