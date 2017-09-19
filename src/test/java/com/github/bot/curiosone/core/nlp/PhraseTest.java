d00a4783db1a185380f67ea0f21778a50747f980
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-19 09:44:42 +0200

    improve Phrase tests
    Reflexive, Symmetric, Transitive and NullComparison test cases for equals and hashCode methods

--- src/test/java/com/github/bot/curiosone/core/nlp/PhraseTest.java
+++ src/test/java/com/github/bot/curiosone/core/nlp/PhraseTest.java
@@ -15,17 +15,55 @@ public class PhraseTest {
   }
 
   @Test
-  public void testEquals() {
-    Phrase classUnderTest = new Phrase("The cat is on the table!");
-    Phrase classUnderTestClone = new Phrase("The cat is on the table!");
-    assertEquals(classUnderTest, classUnderTest);
-    assertEquals(classUnderTest, classUnderTestClone);
-    assertNotEquals(null, classUnderTest);
-    assertNotEquals(new StringBuffer("something different"), classUnderTest);
+  public void testEqualsReflexive() {
+    Phrase p = new Phrase("Fortytwo is a beautiful number.");
+    assertTrue(p.equals(p));
 
-    classUnderTest = new Phrase("This happens with the 42 number");
-    classUnderTestClone = new Phrase("This happens with the 42 number");
-    assertEquals(classUnderTest, classUnderTestClone);
+    p = new Phrase("#42 is the answer!");
+    assertTrue(p.equals(p));
+  }
+
+  @Test
+  public void testEqualsSymmetric() {
+    Phrase p = new Phrase("The cat is on the table.");
+    Phrase pp = new Phrase("The cat is on the table.");
+    assertTrue(p.equals(pp));
+    assertTrue(pp.equals(p));
+
+    p = new Phrase("The dog is sleeping on the couch.");
+    pp = new Phrase("The dog is sleeping on the couch.");
+    assertTrue(p.equals(pp));
+    assertTrue(pp.equals(p));
+  }
+
+  @Test
+  public void testEqualsTransitive() {
+    Phrase p = new Phrase("The transitive property is a useful property in math.");
+    Phrase pp = new Phrase("The transitive property is a useful property in math.");
+    Phrase ppp = new Phrase("The transitive property is a useful property in math.");
+    assertTrue(p.equals(pp));
+    assertTrue(pp.equals(ppp));
+    assertTrue(ppp.equals(p));
+  }
+
+  @Test
+  public void testEqualsNullComparison() {
+    Phrase p = new Phrase("A null value indicates an unset reference.");
+    assertFalse(p.equals(null));
+
+    p = new Phrase("130");
+    assertFalse(p.equals(null));
+  }
+
+  @Test
+  public void testEqualsOtherObj() {
+    Phrase p = new Phrase("Objects have states and behaviors");
+    StringBuffer sb = new StringBuffer("Objects have states and behaviors");
+    assertFalse(p.equals(sb));
+
+    p = new Phrase("An array is a container object that holds a fixed number of values of a single type");
+    String[] as = {"This", "is", "an", "array"};
+    assertFalse(p.equals(as));
   }
 
   @Test
@@ -48,5 +86,24 @@ public class PhraseTest {
 
     p = new Phrase("I l0v3 numb3rs111");
     assertEquals("I l0v3 numb3rs111", p.getText());
+  }
+
+  @Test
+  public void testHashCodeReflexive() {
+    Phrase p = new Phrase("Is my hashCode reflexive?");
+    assertEquals(p.hashCode(), p.hashCode());
+
+    p = new Phrase("I have a reflexive hashCode too!");
+    assertEquals(p.hashCode(), p.hashCode());
+  }
+
+  @Test
+  public void testHashCodeTransitive() {
+    Phrase p = new Phrase("Insert a sentence here...");
+    Phrase pp = new Phrase("Insert a sentence here...");
+    Phrase ppp = new Phrase("Insert a sentence here...");
+    assertEquals(p.hashCode(),pp.hashCode());
+    assertEquals(pp.hashCode(),ppp.hashCode());
+    assertEquals(ppp.hashCode(),p.hashCode());
   }
 }
