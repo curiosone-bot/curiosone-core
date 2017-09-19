200f9817199636262eb111ab312d86c0ec52314a
simonepri <simone.pri@hotmail.it>
2017-09-19 23:09:48 +0200

    Remove unused import

--- src/main/java/com/github/bot/curiosone/core/nlp/Sentence.java
+++ src/main/java/com/github/bot/curiosone/core/nlp/Sentence.java
@@ -5,7 +5,6 @@ import com.github.bot.curiosone.core.util.Interval;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
