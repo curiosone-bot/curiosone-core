7077418e2d88a2cf0ed4d8d6b382a11e36e07f2c
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-19 23:00:10 +0200

    improve ConversationTest

--- src/test/java/com/github/bot/curiosone/core/nlp/ConversationTest.java
+++ src/test/java/com/github/bot/curiosone/core/nlp/ConversationTest.java
@@ -11,43 +11,43 @@ public class ConversationTest {
     String s;
 
     s = Conversation.getAnswer(new Phrase("i am Franco"));
-    assertTrue(s.length() > 0);
-    System.out.println(s);
-
-    s = Conversation.getAnswer(new Phrase("how are you?"));
-    assertTrue(s.length() > 0);
+    assertTrue(s.equals("I will set aside this information")
+        || s.equals("I am glad to know it")
+        || s.equals("This information is not new to me.."));
     System.out.println(s);
 
     s = Conversation.getAnswer(new Phrase("where do you live?"));
-    assertTrue(s.length() > 0);
-    System.out.println(s);
+    assertTrue(s.equals("Right in front of you")
+        || s.equals("Do I live?"));
 
     s = Conversation.getAnswer(new Phrase("tell me a joke"));
-    assertTrue(s.length() > 0);
-    System.out.println(s);
+    assertTrue(
+        s.equals("Can a kangaroo jump higher than a house? Of course, a house does not jump at all.")
+        || s.equals("What do skeletons say before eating?Bone Appetit!"));
 
     s = Conversation.getAnswer(new Phrase("are you stupid?"));
-    assertTrue(s.length() > 0);
-    System.out.println(s);
+    assertTrue(s.equals("Not so much") || s.equals("Siri more than me"));
 
     s = Conversation.getAnswer(new Phrase("are you a human?"));
-    assertTrue(s.length() > 0);
-    System.out.println(s);
+    assertTrue(s.equals("No I am not")
+        || s.equals("Humans have blood, I have 101010101011011"));
 
     s = Conversation.getAnswer(new Phrase("are we friends?"));
-    assertTrue(s.length() > 0);
-    System.out.println(s);
+    assertTrue(s.equals("Facebook teached me what a friend is")
+        ||s.equals("I really want a friend"));
 
     s = Conversation.getAnswer(new Phrase("i want to play a game"));
-    assertTrue(s.length() > 0);
-    System.out.println(s);
+    assertTrue(s.equals("I have different games in my brain, if you want to play, just click on the top-left side button on the screen"));
 
     s = Conversation.getAnswer(new Phrase("can you dance?"));
-    assertTrue(s.length() > 0);
-    System.out.println(s);
+    assertTrue(s.equals("I never learned before, it is very hard for robots")
+        || s.equals("No I am sorry"));
 
     s = Conversation.getAnswer(new Phrase("how it is going?"));
-    assertTrue(s.length() > 0);
-    System.out.println(s);
+    assertTrue(s.equals("Ow.. I do not know")
+        || s.equals("Not so bad, what about you?"));
+
+    s = Conversation.getAnswer(new Phrase("what's the weather like?"));
+    assertTrue(s.equals(""));
   }
 }
