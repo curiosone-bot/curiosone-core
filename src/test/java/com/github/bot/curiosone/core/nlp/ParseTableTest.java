6bc01551ac8ccc38978c0dd349873d128718fb65
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-22 20:59:54 +0200

    Next test (#67)

--- src/test/java/com/github/bot/curiosone/core/nlp/ParseTableTest.java
+++ src/test/java/com/github/bot/curiosone/core/nlp/ParseTableTest.java
@@ -3,9 +3,6 @@ package com.github.bot.curiosone.core.nlp;
 // SUPPRESS CHECKSTYLE AvoidStarImport
 import static org.junit.Assert.*;
 
-// SUPPRESS CHECKSTYLE AvoidStarImport
-import static org.assertj.core.api.Assertions.*;
-
 import java.util.Arrays;
 
 import org.junit.Test;
@@ -16,36 +13,11 @@ public class ParseTableTest {
     ParseTable classUnderTest = new ParseTable(
         Token.tokenize("what is an apple")
     );
-    //System.out.println(classUnderTest);
+    System.out.println(classUnderTest);
 
     classUnderTest = new ParseTable(
         Token.tokenize("what is a golden apple")
     );
-    //System.out.println(classUnderTest);
-  }
-
-  @Test
-  public void testGetCykTableTestOutOfBounds() {
-    ParseTable pt = new ParseTable(Token.tokenize("out of bound"));
-
-    // y < 0
-    assertThatExceptionOfType(IndexOutOfBoundsException.class)
-        .isThrownBy(() -> pt.get(1, -1))
-        .withMessage("Position outside the table [1, -1]");
-
-    // y > size
-    assertThatExceptionOfType(IndexOutOfBoundsException.class)
-        .isThrownBy(() -> pt.get(1, 42))
-        .withMessage("Position outside the table [1, 42]");
-
-    // x < 0
-    assertThatExceptionOfType(IndexOutOfBoundsException.class)
-        .isThrownBy(() -> pt.get(-1, 2))
-        .withMessage("Position outside the table [-1, 2]");
-
-    // x > y
-    assertThatExceptionOfType(IndexOutOfBoundsException.class)
-        .isThrownBy(() -> pt.get(42, 2))
-        .withMessage("Position outside the table [42, 2]");
+    System.out.println(classUnderTest);
   }
 }
--- src/test/java/com/github/bot/curiosone/core/nlp/SpellingTest.java
+++ src/test/java/com/github/bot/curiosone/core/nlp/SpellingTest.java
@@ -8,23 +8,15 @@ import org.junit.Test;
 public class SpellingTest {
   @Test
   public void testGetInstance() {
-    Spelling s = Spelling.getInstance();
-    assertTrue(s instanceof Spelling);
+    Spelling classUnderTest = Spelling.getInstance();
+    assertTrue(classUnderTest instanceof Spelling);
   }
 
   @Test
   public void testCorrect() {
-    Spelling s = Spelling.getInstance();
-    assertEquals("hello", s.correct("hello"));
-    assertEquals("hello", s.correct("helllo"));
-    assertEquals("can", s.correct("cann"));
-    assertEquals("helping", s.correct("heelping"));
-    assertEquals("grow", s.correct("trow"));
-    assertEquals("engineer", s.correct("enginer"));
-    assertEquals("february", s.correct("febraury"));
-    assertEquals("CORRECTME!", s.correct("CORRECTME!"));
-    assertEquals("42", s.correct("42"));
-    assertEquals("accommodate", s.correct("accomodate"));
-    assertEquals("forty", s.correct("fourty"));
+    Spelling classUnderTest = Spelling.getInstance();
+    assertEquals("hello", classUnderTest.correct("hello"));
+    assertEquals("hello", classUnderTest.correct("helllo"));
+    assertEquals("can", classUnderTest.correct("cann"));
   }
 }
