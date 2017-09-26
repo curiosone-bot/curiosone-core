43f8979322ca33dc1ce9d357b7657329edac4bc1
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-26 16:07:54 +0200

    add MessageTest

--- src/test/java/com/github/bot/curiosone/core/workflow/MessageTest.java
+++ src/test/java/com/github/bot/curiosone/core/workflow/MessageTest.java
@@ -15,4 +15,49 @@ public class MessageTest {
     assertEquals("How old are you?", msg.getMessage());
     assertEquals("test", msg.getScope());
   }
+
+  @Test
+  public void testInstantiation() {
+    assertTrue(new Message("Text", "Scope") instanceof Message);
+  }
+
+  @Test
+  public void testGetMessage() {
+    assertEquals("This is the message!",
+        new Message("This is the message!", "This is the scope")
+            .getMessage());
+  }
+
+  @Test
+  public void testGetScope() {
+    assertEquals("This is the scope",
+        new Message("This is the message!", "This is the scope")
+            .getScope());
+  }
+
+  @Test
+  public void testToString() {
+    assertEquals("Text (Scope)", new Message("Text", "Scope").toString());
+  }
+
+  @Test
+  public void testEquals() {
+    Message br = new Message("Text", "Scope");
+    assertTrue(br.equals(br));
+    assertFalse(br.equals(null));
+    assertFalse(br.equals(new Integer(42)));
+    Message brr = new Message("Text", "Scope");
+    assertTrue(br.equals(brr));
+    brr = new Message("text", "scope");
+    assertFalse(br.equals(brr));
+  }
+
+  @Test
+  public void testHashCode() {
+    assertEquals(new Message("Happy", "Scope.of(Happy)").hashCode(),
+        new Message("Happy", "Scope.of(Happy)").hashCode());
+
+    assertNotEquals(new Message("Different", "Message").hashCode(),
+        new Message("Test", "hashCode Scope"));
+  }
 }
