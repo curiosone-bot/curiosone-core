19ebffb74685d9af3c3254ba923915d283fb2950
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-26 15:38:36 +0200

    improve BrainResponseTest

--- src/test/java/com/github/bot/curiosone/core/extraction/BrainResponseTest.java
+++ src/test/java/com/github/bot/curiosone/core/extraction/BrainResponseTest.java
@@ -1,9 +1,54 @@
 package com.github.bot.curiosone.core.extraction;
 
+// SUPPRESS CHECKSTYLE AvoidStarImport
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class BrainResponseTest {
 
   @Test
   public void testInstantiation() {
     assertTrue(new BrainResponse("Text", "Scope") instanceof BrainResponse);
+  }
+
+  @Test
+  public void testGetMessage() {
+    assertEquals("This is the message!",
+        new BrainResponse("This is the message!", "This is the scope")
+            .getMessage());
+  }
+
+  @Test
+  public void testGetScope() {
+    assertEquals("This is the scope",
+        new BrainResponse("This is the message!", "This is the scope")
+            .getScope());
+  }
+
+  @Test
+  public void testToString() {
+    assertEquals("Text(Scope)", new BrainResponse("Text", "Scope").toString());
+  }
+
+  @Test
+  public void testEquals() {
+    BrainResponse br = new BrainResponse("Text", "Scope");
+    assertTrue(br.equals(br));
+    assertFalse(br.equals(null));
+    assertFalse(br.equals(new Integer(42)));
+    BrainResponse brr = new BrainResponse("Text", "Scope");
+    assertTrue(br.equals(brr));
+    brr = new BrainResponse("text", "scope");
+    assertFalse(br.equals(brr));
+  }
+
+  @Test
+  public void testHashCode() {
+    assertEquals(new BrainResponse("Happy", "Scope.of(Happy)").hashCode(),
+        new BrainResponse("Happy", "Scope.of(Happy)").hashCode());
+
+    assertNotEquals(new BrainResponse("Different", "BrainResponse").hashCode(),
+        new BrainResponse("Test", "hashCode Scope"));
   }
 }
