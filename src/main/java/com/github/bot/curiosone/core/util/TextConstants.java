b007fd478d7d4f72b0adbb938fd2289b9bb00315
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-27 22:08:37 +0200

    add TextContants class

---
+++ src/main/java/com/github/bot/curiosone/core/util/TextConstants.java
@@ -0,0 +1,31 @@
+package com.github.bot.curiosone.core.util;
+
+public class TextConstants {
+
+  /**
+   * String representation of the IllegalArgumentException message thrown in Meaning.setFrequency()
+   * method.
+   */
+  public static final String FREQ_MUST_BE_POS_ERR = "Frequency must be positive";
+
+  /**
+   * String representation of the IndexOutOfBoundsException message thrown in ParseTable.get()
+   * method.
+   */
+  public static final String POS_OUTSIDE_ERR = "Position outside the table";
+
+  /**
+   * Stores openening square bracket symbol.
+   */
+  public static final String OPEN_SQ_BRACKET = "[";
+
+  /**
+   * Stores closing square bracket symbol.
+   */
+  public static final String CLOSE_SQ_BRACKET = "]";
+
+  /**
+   * Stores the String representation for the value separator used in toString methods.
+   */
+  public static final String COMMA_SEP = ", ";
+}
