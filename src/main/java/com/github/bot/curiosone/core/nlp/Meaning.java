94a00ac5bc1879f9e9e36ac2162dc2f623521b27
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-27 21:28:40 +0200

    improve Meaning javadoc and codestyle

--- src/main/java/com/github/bot/curiosone/core/nlp/Meaning.java
+++ src/main/java/com/github/bot/curiosone/core/nlp/Meaning.java
@@ -6,20 +6,46 @@ import java.util.Objects;
  * A grammatical Meaning of a group of words.
  */
 public class Meaning implements Comparable<Meaning> {
-  /** The part of speech type of the meaning. */
+
+  /**
+   * String representation of the IllegalArgumentException message thrown in setFrequency method.
+   */
+  private static final String FREQ_MUST_BE_POS_ERR = "Frequency must be positive";
+
+  /**
+   * Stores openening square bracket symbol.
+   */
+  private static final String OPEN_SQ_BRACKET = "[";
+
+  /**
+   * Stores closing square bracket symbol.
+   */
+  private static final String CLOSE_SQ_BRACKET = "]";
+
+  /**
+   * Stores the String representation for the value separator used in toString method.
+   */
+  private static final String COMMA_SEP = ", ";
+
+  /**
+   * The part of speech type of the meaning.
+   */
   POS pos;
 
-  /** The lexical category of the meaning. */
+  /**
+   * The lexical category of the meaning.
+   */
   LEX lex;
 
-  /** The frequency of the meaning. */
+  /**
+   * The frequency of the meaning.
+   */
   int freq;
 
   /**
-   * Constructor of a Meaning.
-   *
-   * @param pos the part of speech type of the new meaning
-   * @param lex the lexical category of the new meaning
+   * Constructs this Meaning instance.
+   * @param pos the part of speech type for this new meaning
+   * @param lex the lexical category for this new meaning
    */
   public Meaning(POS pos, LEX lex) {
     this.pos = pos;
@@ -28,59 +54,49 @@ public class Meaning implements Comparable<Meaning> {
   }
 
   /**
-   * Gets the part of speech type of this meaning.
-   *
-   * @return the part of speech type of this meaning
+   * Gets the part of speech type for this meaning.
    */
   public POS getPOS() {
     return pos;
   }
 
   /**
-   * Gets the lexical category of this meaning.
-   *
-   * @return the lexical category of this meaning
+   * Gets the lexical category for this meaning.
    */
   public LEX getLEX() {
     return lex;
   }
 
   /**
-   * Gets how much this meaning is frequent.
-   *
-   * @return a positive frequency of this meaning.
+   * Gets frequency for this meaning.
    */
   public int getFrequency() {
     return freq;
   }
 
   /**
-   * Sets how much this meaning is frequent.
-   *
-   * @param frequency a positive frequency to set
+   * Sets the frewuency for this meaning.
+   * @param frequency the frequency value to be set
    * @throws IllegalArgumentException if negative frequency is passed
    */
   public void setFrequency(int frequency) {
     if (frequency < 0) {
-      throw new IllegalArgumentException("Frequency must be positive");
+      throw new IllegalArgumentException(FREQ_MUST_BE_POS_ERR);
     }
     freq = frequency;
   }
 
   /**
-   * Returns a string representation of this meaning.
-   *
-   * @return a string representation of this meaning in the form [pos, lex, frequency]
+   * Returns a string representation of this meaning in the form [pos, lex, frequency].
    */
   @Override
   public String toString() {
-    return "[" + pos + ", " + lex + ", " + freq + "]";
+    return OPEN_SQ_BRACKET + pos + COMMA_SEP + lex + COMMA_SEP + freq + CLOSE_SQ_BRACKET;
   }
 
   /**
    * Compares this meaning to the specified object.
-   *
-   * @param  other the other meaning
+   * @param  other the other meaning to be compared against
    * @return {@code true} if this meaning equals the other meaning;
    *         {@code false} otherwise
    */
@@ -97,9 +113,7 @@ public class Meaning implements Comparable<Meaning> {
   }
 
   /**
-   * Returns an integer hash code for this meaning.
-   *
-   * @return an integer hash code for this meaning
+   * Returns the HashCode for this Meaning.
    */
   @Override
   public int hashCode() {
@@ -107,9 +121,8 @@ public class Meaning implements Comparable<Meaning> {
   }
 
   /**
-   * Compares this meaning to the other meaning.
-   *
-   * @param other the other meaning
+   * Compares this Meaning to the other meaning.
+   * @param other the other Meaning to be compared against
    * @return a negative value if this is ordered before other;
    *         a positive value if this is ordered after other;
    *         zero if this and other are ordered together.
