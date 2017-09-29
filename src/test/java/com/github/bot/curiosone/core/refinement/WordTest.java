9370ef1600645178030fbf7e0d3c725b6ea5264c
Claudio Venanzi <cl124c41a@gmail.com>
2017-09-30 00:45:25 +0200

    added WordTest

---
+++ src/test/java/com/github/bot/curiosone/core/refinement/WordTest.java
@@ -0,0 +1,18 @@
+package com.github.bot.curiosone.core.refinement;
+
+import static org.junit.Assert.assertTrue;
+
+import it.uniroma1.lcl.babelmorph.POS;
+import it.uniroma1.lcl.babelnet.data.BabelPOS;
+
+import org.junit.Test;
+
+public class WordTest {
+
+  @Test
+  public void main() {
+    assertTrue(WordPart.from(BabelPOS.ADVERB).equals(WordPart.from(POS.ADVERB)));
+    assertTrue(WordPart.from(BabelPOS.NOUN).equals(WordPart.Noun));
+    assertTrue(WordPart.from(POS.VERB).equals(WordPart.Verb));
+  }
+}
