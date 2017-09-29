26915930379a3cafdff3525e77b83d624cf1f214
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-09-29 11:14:14 +0200

    improve SpellingTest and use AssertJ

--- src/test/java/com/github/bot/curiosone/core/nlp/SpellingTest.java
+++ src/test/java/com/github/bot/curiosone/core/nlp/SpellingTest.java
@@ -1,5 +1,6 @@
 package com.github.bot.curiosone.core.nlp;
 
+import static org.assertj.core.api.Assertions.assertThat;
 // SUPPRESS CHECKSTYLE AvoidStarImport
 import static org.junit.Assert.*;
 
@@ -8,15 +9,35 @@ import org.junit.Test;
 public class SpellingTest {
   @Test
   public void testGetInstance() {
-    Spelling classUnderTest = Spelling.getInstance();
-    assertTrue(classUnderTest instanceof Spelling);
+    Spelling s = Spelling.getInstance();
+    assertThat(s instanceof Spelling).isTrue();
   }
 
   @Test
   public void testCorrect() {
-    Spelling classUnderTest = Spelling.getInstance();
-    assertEquals("hello", classUnderTest.correct("hello"));
-    assertEquals("hello", classUnderTest.correct("helllo"));
-    assertEquals("can", classUnderTest.correct("cann"));
+    Spelling s = Spelling.getInstance();
+    assertThat(s.correct("hello")).isEqualTo("hello");
+
+    assertThat(s.correct("hel lo")).isEqualTo("hello");
+
+    assertThat(s.correct("hello,")).isEqualTo("hello");
+
+    assertThat(s.correct(",hello")).isEqualTo("hello");
+
+    assertThat(s.correct("helllo")).isEqualTo("hello");
+
+    assertThat(s.correct("cann")).isEqualTo("can");
+
+    assertThat(s.correct("ca.n")).isEqualTo("can");
+
+    assertThat(s.correct("ca3n")).isEqualTo("can");
+
+    assertThat(s.correct("c4n")).isEqualTo("can");
+
+    assertThat(s.correct("can1")).isEqualTo("can");
+
+    assertThat(s.correct("!can")).isEqualTo("can");
+
+    assertThat(s.correct("united kingdom")).isEqualTo("united kingdom");
   }
 }
