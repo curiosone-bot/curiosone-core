677fc067bd748aaedf44695964e57ae28cf27b2d
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-10-02 23:35:18 +0200

    fix Dictionary file path

--- src/main/java/com/github/bot/curiosone/core/analysis/DictionaryLoader.java
+++ src/main/java/com/github/bot/curiosone/core/analysis/DictionaryLoader.java
@@ -1,5 +1,10 @@
 package com.github.bot.curiosone.core.analysis;
 
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
@@ -17,27 +22,30 @@ public class DictionaryLoader {
   /**
    *The Uri containing the .properties file.
    */
-  private static final String uri = "resources/Dictionary/DataDict.properties";
-  
+  private static final String uri = "/dictionary/DataDict.properties";
+
   /**
    * The map which contains words and their sentiment's score.
    */
-  private static Map<String, Double> dict = new HashMap<>();
-  
+  private static Map<String, Double> dict;
+
   /**
    * Initializing of the properties.
    */
   private static Properties properties = new Properties();
-  
+
 
   /**
    * Loading the .properties file in the dictionary.
    * @Return the loaded dictionary
    */
   public static Map<String, Double> loadDict() {
+    Path path = null;
     try {
-      properties.load(new FileInputStream(uri));
-    } catch (IOException e) {
+      URL resource = DictionaryLoader.class.getResource(uri);
+      path = Paths.get(resource.toURI());
+      properties.load(new FileInputStream(path.toString()));
+    } catch (IOException|URISyntaxException e) {
       e.printStackTrace();
     }
     for (String key : properties.stringPropertyNames()) {
@@ -45,5 +53,5 @@ public class DictionaryLoader {
     }
     return dict;
   }
-  
+
 }
