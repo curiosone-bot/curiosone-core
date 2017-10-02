5612766f4bce09e299f3a042ff62a5815c7dee66
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-10-02 09:32:05 +0200

    improve Word javadoc

--- src/main/java/com/github/bot/curiosone/core/nlp/Word.java
+++ src/main/java/com/github/bot/curiosone/core/nlp/Word.java
@@ -5,24 +5,31 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
- * A Word is like a Token but with a restricted set of meanings.
+ * Handles a Word.
+ * A Word is like a Token, but with a restricted set of meanings.
  */
 public class Word {
-  /** The text rappresentation of this word. */
+
+  /**
+   * Stores the textual representation of this Word.
+   */
   String text;
 
-  /** Base form of the world. */
+  /**
+   * Stores the base form of the word.
+   */
   String lemma;
 
-  /** Meanings of the word. */
+  /**
+   * Stores all the possible meanings of the word.
+   */
   Set<Meaning> means;
 
   /**
-   * Constructs a Word starting from a text.
-   *
-   * @param text [description]
-   * @param lemma [description]
-   * @param means [description]
+   * Constructs a Word starting from a text, a lemma and a Set of meanings.
+   * @param text a textual representation of this Word
+   * @param lemma the lemma for this Word
+   * @param means Set containing all the possible meanings for this Word
    */
   public Word(String text, String lemma, Set<Meaning> means) {
     this.text = text;
@@ -31,11 +38,10 @@ public class Word {
   }
 
   /**
-   * Constructs a Word starting from a text.
-   *
-   * @param text [description]
-   * @param lemma [description]
-   * @param mean [description]
+   * Constructs a Word starting from a text, a lemma and a single meaning.
+   * @param text textual representation of this Word
+   * @param lemma the lemma for this Word
+   * @param mean meaning of this Word
    */
   public Word(String text, String lemma, Meaning mean) {
     this.text = text;
@@ -45,45 +51,39 @@ public class Word {
   }
 
   /**
-   * Returns the word.
-   *
-   * @return the word.
+   * Returns the textual representation of this Word.
    */
   public String getText() {
     return text;
   }
 
   /**
-   * Returns the lemma.
-   *
-   * @return the lemma.
+   * Returns the lemma of this Word.
    */
   public String getLemma() {
     return lemma;
   }
 
   /**
-   * Returns the set of the meanings of this word.
-   *
-   * @return the set of the meanings of this word.
+   * Returns the Set of the meanings of this Word.
    */
   public Set<Meaning> getMeanings() {
     return means;
   }
 
   /**
-   * itMeans description.
-   * @param  mean [description]
-   * @return [description]
+   * Checks whether this Word has the given meaning or not.
+   * @param  mean the meaning to be checked
+   * @return {@code true} if the given meaning is associated with this Word, {@code false} otherwise
    */
   public boolean itMeans(Meaning mean) {
     return means.contains(mean);
   }
 
   /**
-   * itMeans description.
-   * @param  pos [description]
-   * @return [description]
+   * Checks whether this Word is the given Part of Speech Type or not.
+   * @param  pos the POS to be checked
+   * @return {@code true} if this Word is the given POS, {@code false} otherwise
    */
   public boolean itMeans(POS pos) {
     return means.stream()
@@ -93,9 +93,9 @@ public class Word {
   }
 
   /**
-   * itMeans description.
-   * @param  lex [description]
-   * @return [description]
+   * Checks whether this Word is the given Lexical Type or not.
+   * @param lex the LEX to be checked
+   * @return {@code true} if this Word is the given LEX, {@code false} otherwise
    */
   public boolean itMeans(LEX lex) {
     return means.stream()
@@ -105,9 +105,7 @@ public class Word {
   }
 
   /**
-   * Returns a string representation of this word.
-   *
-   * @return a string representation of this word in the form [text, word, meanings]
+   * Returns a String representation of this Word in the form [text, word, meanings].
    */
   @Override
   public String toString() {
@@ -115,10 +113,9 @@ public class Word {
   }
 
   /**
-   * Compares this word to the specified object.
-   *
-   * @param  other the other word
-   * @return {@code true} if this word equals the other word;
+   * Checks whether this Word equals to the given Object.
+   * @param other the other Word to be compared against
+   * @return {@code true} if this Word equals the other Word;
    *         {@code false} otherwise
    */
   @Override
@@ -134,9 +131,8 @@ public class Word {
   }
 
   /**
-   * Returns an integer hash code for this word.
-   *
-   * @return an integer hash code for this word
+   * Returns the HashCode of this Word.
+   * The HashCode is based on the HashCode of the textual representation of this Word.
    */
   @Override
   public int hashCode() {
