b282ac39a2d04a713645f14d10996fb0acdebd88
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-10-04 00:01:19 +0200

    improve TokenScorerTest

--- src/test/java/com/github/bot/curiosone/analysis/TokenScorerTest.java
+++ src/test/java/com/github/bot/curiosone/analysis/TokenScorerTest.java
@@ -16,59 +16,59 @@ public class TokenScorerTest {
     List<Token> lt = Token.tokenize("The cat is on the car and the dog is on the table.");
     Token t = Token.tokenize("I am happy").get(2);
 
-    assertThat(ts.calculateScore(t)).isEqualTo(0.5625);
+    assertThat(ts.calculateScore(t)).isPositive();
 
     t = lt.get(1);
-    assertThat(ts.calculateScore(t)).isEqualTo(-0.0125);
+    assertThat(ts.calculateScore(t)).isNegative();
 
     t = lt.get(5);
-    assertThat(ts.calculateScore(t)).isEqualTo(0.0);
+    assertThat(ts.calculateScore(t)).isZero();
 
     t = lt.get(9);
-    assertThat(ts.calculateScore(t)).isEqualTo(0.0);
+    assertThat(ts.calculateScore(t)).isZero();
 
     lt = Token.tokenize("Watch your mouth! Speak politely! None should be emotionally upset!");
     t = lt.get(0);
-    assertThat(ts.calculateScore(t)).isEqualTo(0.057692307692307696);
+    assertThat(ts.calculateScore(t)).isPositive();
 
     t = lt.get(2);
-    assertThat(ts.calculateScore(t)).isEqualTo(0.0);
+    assertThat(ts.calculateScore(t)).isZero();
 
     t = lt.get(3);
-    assertThat(ts.calculateScore(t)).isEqualTo(0.0);
+    assertThat(ts.calculateScore(t)).isZero();
 
     t = lt.get(4);
-    assertThat(ts.calculateScore(t)).isEqualTo(0.125);
+    assertThat(ts.calculateScore(t)).isPositive();
 
     t = lt.get(5);
-    assertThat(ts.calculateScore(t)).isEqualTo(-0.28125);
+    assertThat(ts.calculateScore(t)).isNegative();
 
     t = lt.get(8);
-    assertThat(ts.calculateScore(t)).isEqualTo(0.0625);
+    assertThat(ts.calculateScore(t)).isPositive();
 
     t = lt.get(9);
-    assertThat(ts.calculateScore(t)).isEqualTo(-0.23529411764705882);
+    assertThat(ts.calculateScore(t)).isNegative();
   }
 
   @Test
   public void testCalculateScoreListToken() {
     TokenScorer ts = new TokenScorer();
     List<Token> lt = Token.tokenize("I am very happy today!");
-    assertThat(ts.calculateScore(lt)).isGreaterThan(0.0);
+    assertThat(ts.calculateScore(lt)).isPositive();
 
     lt = Token.tokenize("I am very happy today! I feel ok.");
-    assertThat(ts.calculateScore(lt)).isGreaterThan(0.0);
+    assertThat(ts.calculateScore(lt)).isPositive();
 
     lt = Token.tokenize("I am very happy today! I feel ok. Happiness is crucial for peace of mind");
-    assertThat(ts.calculateScore(lt)).isGreaterThan(0.0);
+    assertThat(ts.calculateScore(lt)).isPositive();
 
     lt = Token.tokenize("Let's go negative, down down down negative negative score");
-    assertThat(ts.calculateScore(lt)).isLessThan(0.0);
+    assertThat(ts.calculateScore(lt)).isNegative();
 
     lt = Token.tokenize("That's a sad story :(");
-    assertThat(ts.calculateScore(lt)).isLessThan(0.0);
+    assertThat(ts.calculateScore(lt)).isNegative();
 
     lt = Token.tokenize("Microfossil curbstone");
-    assertThat(ts.calculateScore(lt)).isEqualTo(0.0);
+    assertThat(ts.calculateScore(lt)).isZero();
   }
 }
