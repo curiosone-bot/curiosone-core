37c9f36df04546ea1281b570adfd34497355cc44
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-10-03 21:31:33 +0200

    add EmotionAnalysis javadoc

--- src/main/java/com/github/bot/curiosone/core/analysis/EmotionAnalysis.java
+++ src/main/java/com/github/bot/curiosone/core/analysis/EmotionAnalysis.java
@@ -3,14 +3,21 @@ package com.github.bot.curiosone.core.analysis;
 import com.github.bot.curiosone.core.nlp.Phrase;
 
 /**
- * Desc.
+ * Handles emotion analysis for a Phrase.
+ * Provides a single public static method to perform to calculate the emotion of a given Phrase.
  */
 public class EmotionAnalysis {
+
+  /**
+   * Private constructor.
+   */
   private EmotionAnalysis() {}
 
   /**
-   * getEmotion description.
-   * @param p [description]
+   * Performs an emotion analysis of the provided Phrase.
+   * @param p the Phrase to be analysed.
+   * @return a String representation of the calculated emotion.
+   * Currently, supports "sad", "happy", "angry" and "neutral" feelings.
    */
   public static String getEmotion(Phrase p) {
     //TODO: Add implementation.
