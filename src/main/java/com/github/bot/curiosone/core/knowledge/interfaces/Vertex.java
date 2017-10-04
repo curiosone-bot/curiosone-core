a32bcdda92ab93f5e0a33a9ab6baa1c080dcd319
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-10-04 10:31:52 +0200

    improve Vertex javadoc

--- src/main/java/com/github/bot/curiosone/core/knowledge/interfaces/Vertex.java
+++ src/main/java/com/github/bot/curiosone/core/knowledge/interfaces/Vertex.java
@@ -1,13 +1,12 @@
 package com.github.bot.curiosone.core.knowledge.interfaces;
 
 /**
- * Intherface for SemanticNetwork Concept.
- * @author Sordi
+ * Represents a Vertex.
  */
 public interface Vertex {
+
   /**
-   * Method that returns Vertex ID's.
-   * @return Vertex ID.
+   * Returns a String representation of the ID of the Vertex.
    */
   String getId();
 }
