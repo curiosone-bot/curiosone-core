e79865dc999121b263bb0f2c344309a7a1ae38f7
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-10-08 15:52:54 +0200

    improve QuestionTest

--- src/test/java/com/github/bot/curiosone/core/extraction/QuestionTest.java
+++ src/test/java/com/github/bot/curiosone/core/extraction/QuestionTest.java
@@ -2,16 +2,29 @@ package com.github.bot.curiosone.core.extraction;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.github.bot.curiosone.core.knowledge.SemanticNetwork;
+import com.github.bot.curiosone.core.knowledge.SemanticRelationType;
 import com.github.bot.curiosone.core.nlp.Phrase;
 import com.github.bot.curiosone.core.nlp.Sentence;
+
+import java.io.IOException;
 
 import org.junit.Test;
 
 public class QuestionTest {
 
   @Test
-  public void testGetAnswer() {
-    Sentence s = Sentence.extract(new Phrase("Who is Gogol?")).get(0);
+  public void testGetAnswer() throws IOException {
+    SemanticNetwork sn = SemanticNetwork.getInstance();
+    sn.learn("abcdedfgh", SemanticRelationType.IS_A, "alphabet");
+
+    Sentence s = Sentence.extract(new Phrase("Who is abcdedfgh?")).get(0);
+    assertThat(Question.getAnswer(s, "")).isPresent();
+
+    s = Sentence.extract(new Phrase("Where is abcdedfgh?")).get(0);
+    assertThat(Question.getAnswer(s, "")).isPresent();
+
+    s = Sentence.extract(new Phrase("Who is Gogol?")).get(0);
     assertThat(Question.getAnswer(s, "")).isPresent();
 
     s = Sentence.extract(new Phrase("Where is Gogol?")).get(0);
