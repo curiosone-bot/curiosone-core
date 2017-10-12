9806b9b58928623118fbe6f1a3290ca6bac6fbe3
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-10-12 08:56:27 +0200

    improve ClauseMain javadoc

--- src/main/java/com/github/bot/curiosone/core/refinement/ClauseMain.java
+++ src/main/java/com/github/bot/curiosone/core/refinement/ClauseMain.java
@@ -2,37 +2,61 @@ package com.github.bot.curiosone.core.refinement;
 
 import com.github.bot.curiosone.core.refinement.interfaces.Clause;
 
+/**
+ * Represents a semantically refined Clause.
+ * Provides methods to create the semantically refined Clause and to retrieve its contents, such as
+ * the subject and the predicate.
+ * @see  Clause The Clause Interface
+ */
 public class ClauseMain implements Clause {
 
-  private PhraseNoun subject;
-  private PhraseVerb predicate;
-  
   /**
-   * Clause constructor.
-   * @param subject subject
-   * @param predicate predicate
+   * Stores the subject of this semantically refined Clause.
+   */
+  private PhraseNoun subject;
+
+  /**
+   * Stores the predicate of this semantically refined Clause.
+   */
+  private PhraseVerb predicate;
+
+  /**
+   * Constructs a semantically refined Clause with a given subject and its predicate.
+   * @param  subject
+   *         the subject of this semantically refined Clause
+   * @param  predicate
+   *         the predicate of this semantically refined Clause
+   * @see  PhraseNoun The PhraseNoun class
+   * @see  PhraseVerb The PhraseVerb class
    */
   public ClauseMain(PhraseNoun subject, PhraseVerb predicate) {
     this.subject   = subject;
     this.predicate = predicate;
   }
-  
+
   /**
-   * Returns the subject phrase.
-   * @return phrase
+   * Gets the subject of this semantically refined Clause.
+   * @return  the subject of this semantically refined Clause
+   * @see  PhraseNoun The PhraseNoun Class
    */
   public PhraseNoun getSubject() {
     return subject;
   }
 
   /**
-   * Returns the predicate phrase.
-   * @return phrase
+   * Gets the predicate of this semantically refined Clause.
+   * @return  the predicate of this semantically refined Clause
+   * @see  PhraseVerb The PhraseVerb Class
    */
   public PhraseVerb getPredicate() {
     return predicate;
   }
 
+  /**
+   * Returns a String representation of this semantically refined Clause, in the form:
+   * [subject predicate].
+   * @return  a String representation of this semantically refined Clause
+   */
   @Override
   public String toString() {
     return subject.toString() + " " + predicate.toString();
