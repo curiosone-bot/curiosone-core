8c0cab2c728465e2e9010e1ffccc01136f25adfe
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-10-12 08:50:18 +0200

    improve Phrase javadoc

--- src/main/java/com/github/bot/curiosone/core/refinement/interfaces/Phrase.java
+++ src/main/java/com/github/bot/curiosone/core/refinement/interfaces/Phrase.java
@@ -1,10 +1,14 @@
 package com.github.bot.curiosone.core.refinement.interfaces;
 
+/**
+ * Represents a Phrase.
+ * Provides a method to get a String representation of the Phrase.
+ */
 public interface Phrase {
-  
+
   /**
-   * Refinement logic.
-   * @return refined phrase
+   * Returns a String representation of the Phrase.
+   * @return  a String representation of the Phrase
    */
   @Override String toString();
 
