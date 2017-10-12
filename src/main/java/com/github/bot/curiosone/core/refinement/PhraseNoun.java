97872faf986a216754e77c52dab08ec7741d8dc1
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-10-12 08:58:36 +0200

    improve PhraseNoun javadoc

--- src/main/java/com/github/bot/curiosone/core/refinement/PhraseNoun.java
+++ src/main/java/com/github/bot/curiosone/core/refinement/PhraseNoun.java
@@ -4,12 +4,25 @@ import com.github.bot.curiosone.core.refinement.interfaces.Phrase;
 
 public class PhraseNoun implements Phrase {
 
+  /**
+   * Stores the Noun of the Phrase.
+   */
   private WordNoun main;
-  
+
+  /**
+   * Constructs this PhraseNoun with a given Noun.
+   * @param  WordNoun
+   *         the  given Noun
+   * @see  WordNoun The WordNoun Class
+   */
   public PhraseNoun(WordNoun main) {
     this.main = main;
   }
-  
+
+  /**
+   * Returns a String representation of this PhraseNoun.
+   * @return  a String representation of this PhraseNoun
+   */
   @Override
   public String toString() {
     return main.toString();
