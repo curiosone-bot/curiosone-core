6be25fbdf8d5a21cc428651f023d983944e1fb34
Daniele Solombrino <daniele.solombrino@gmail.com>
2017-10-14 08:45:22 +0200

    add WordVerbTest

---
+++ src/test/java/com/github/bot/curiosone/core/refinement/WordVerbTest.java
@@ -0,0 +1,35 @@
+package com.github.bot.curiosone.core.refinement;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import com.github.bot.curiosone.core.refinement.interfaces.Word;
+
+import org.junit.Test;
+
+public class WordVerbTest {
+
+  @Test
+  public void testInstantiation() {
+    WordVerb wv = new WordVerb("Eat", TypeVerb.PresentP2);
+    assertThat(wv instanceof Word).isTrue();
+  }
+
+  @Test
+  public void testToString() {
+    WordVerb wv = new WordVerb("Eaten", TypeVerb.ParticiplePastP1);
+    assertThat(wv.toString()).isNotNull().isNotEmpty().isNotEqualTo("is").isNotEqualTo("am");
+    assertThatCode(() -> new WordVerb("Eaten", TypeVerb.ParticiplePastP1).toString())
+        .doesNotThrowAnyException();
+
+    wv = new WordVerb("be", TypeVerb.PresentS1);
+    assertThat(wv.toString()).isNotNull().isNotEmpty().isEqualTo("am");
+
+    wv = new WordVerb("be", TypeVerb.PresentS3);
+    assertThat(wv.toString()).isNotNull().isNotEmpty().isEqualTo("is");
+
+    wv = new WordVerb("djshckjsdhckjsdck", TypeVerb.PresentS2);
+    assertThat(wv.toString()).isNotNull().isNotEmpty().isEqualTo("djshckjsdhckjsdck");
+  }
+}
